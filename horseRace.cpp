8463fa75f88a7add85f45c8c612400f08d5232e5
jkfletcher98 <jkfletcher@bsu.edu>
2025-01-28 15:23:18 +0000

    Started code

---
+++ horseRace.cpp
@@ -0,0 +1,11 @@
+const int TOTAL_HORSES = 5;
+const int TRACK_LENGTH = 15;
+
+int main(){
+	bool keepGoing = true;
+	while (keepGoing){
+		for (horse in TOTAL_HORSES){
+			advance();
+			printlLane();
+		} // end for loop
+	} // end while loop
