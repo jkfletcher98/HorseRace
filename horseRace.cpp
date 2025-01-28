a49ab2b84d22f41863af3b3c38435c3c52c5002e
jkfletcher98 <jkfletcher@bsu.edu>
2025-01-28 15:35:58 +0000

    Added advance function

--- horseRace.cpp
+++ horseRace.cpp
@@ -1,3 +1,7 @@
+#include <random>
+std::random_device rd;
+std::uniform_int_ditribution<int> dist(0, 1);
+
 const int TOTAL_HORSES = 5;
 const int TRACK_LENGTH = 15;
 
@@ -6,6 +10,17 @@ int main(){
 	while (keepGoing){
 		for (horse in TOTAL_HORSES){
 			advance();
-			printlLane();
+			printLane();
 		} // end for loop
 	} // end while loop
+} // end main
+
+int advance(horsePosition, horseNum){
+	number = dist(rd);
+	horseNum += number;
+	return horseNum;
+} // end advance
+
+int printLane(
+
+
