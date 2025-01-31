57c5b1a7b8356be011c4d0f7093f7af39e69a2f8
jkfletcher98 <jkfletcher@bsu.edu>
2025-01-31 14:51:44 +0000

    Updated horseRace.cpp

--- horseRace.cpp
+++ horseRace.cpp
@@ -6,21 +6,28 @@ const int TOTAL_HORSES = 5;
 const int TRACK_LENGTH = 15;
 
 int main(){
+	int horses[] = {0, 0, 0, 0, 0};
 	bool keepGoing = true;
 	while (keepGoing){
-		for (horse in TOTAL_HORSES){
-			advance();
-			printLane();
-		} // end for loop
-	} // end while loop
+		for (int hn = 0; hn < TOTAL_HORSES; hn++){
+			advance(hn, horses);
+			printLane(hn, horses);
+			if (isWinner(hn, horses)){
+				keepGoing = false;
+			} // end if
+		} // end for
+		std::cout << "Press ENTER for next round.";
+		std::cin:ignore;
+	} // end while
 } // end main
 
-int advance(horsePosition, horseNum){
-	number = dist(rd);
-	horseNum += number;
-	return horseNum;
+int advance(hn, horses){
+	coin = dist(rd);
+	horse += coin;
+	return horse;
 } // end advance
 
-int printLane(
-
-
+int printLane(hn, horses){
+	for (int space = 0; space < TRACK_LENGTH; space++){
+		if (space == horse){
+			printf("%i" /
